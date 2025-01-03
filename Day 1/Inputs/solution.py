4d4b8caa56d8f1ead1b2ef2023f9a0f90034614b
abdulraheem48 <91835360+abdulraheem48@users.noreply.github.com>
2025-01-03 12:34:31 +0530

    Create solution.py
    solution for task.md

---
+++ Day 1/Inputs/solution.py
@@ -0,0 +1,7 @@
+# PAUSE 1. Update the code to add an exclamation mark Using what you have learnt in this lesson and previous, can you figure out how take user input and slot it in between 2 strings?
+# You are looking to print a sentence like this:
+
+# Hello Name!
+# e.g. Hello Angela!
+
+print("Hello " + input("What is your name?") + "!")
